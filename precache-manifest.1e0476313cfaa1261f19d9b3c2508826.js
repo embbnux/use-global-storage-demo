0b27baf7c10855ddc77b2717ee2e52c9fd8bd6ae
embbnux <embbnux@embbnux.com>
2019-10-30 11:01:29 +0800

    upgrade

---
+++ precache-manifest.1e0476313cfaa1261f19d9b3c2508826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "929127469ee733c2ea8c",
+    "url": "/use-global-storage-demo/static/js/runtime~main.1798df7f.js"
+  },
+  {
+    "revision": "80a2509208ed0e08f29b",
+    "url": "/use-global-storage-demo/static/js/main.83ecdc29.chunk.js"
+  },
+  {
+    "revision": "5c78c3e92ea0d3c1767c",
+    "url": "/use-global-storage-demo/static/js/2.6bd70bc0.chunk.js"
+  },
+  {
+    "revision": "80a2509208ed0e08f29b",
+    "url": "/use-global-storage-demo/static/css/main.b139bda7.chunk.css"
+  },
+  {
+    "revision": "c368a678b5ddcc5de58174a71dbd09b0",
+    "url": "/use-global-storage-demo/index.html"
+  }
+];
